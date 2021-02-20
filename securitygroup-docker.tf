58a867c53f717e3ba74e3552f4624e3d6d7c382c
Griffin2002 <31487199+Griffin2002@users.noreply.github.com>
2021-02-19 23:21:27 -0500

    update instance name and formatting

--- securitygroup-docker.tf
+++ securitygroup-docker.tf
@@ -1,5 +1,5 @@
 resource "openstack_networking_secgroup_v2" "rancher_docker" {
-  name = "rancher_docker"
+  name        = "rancher_docker"
   description = "security group for docker deployed versions of rancher"
 }
 
@@ -7,38 +7,38 @@ resource "openstack_networking_secgroup_rule_v2" "rancher_ui" {
   direction         = "ingress"
   ethertype         = "tcp"
   security_group_id = openstack_networking_secgroup_v2.rancher_docker.id
-  port_range_max = 80
-  port_range_min = 80
+  port_range_max    = 80
+  port_range_min    = 80
 }
 
 resource "openstack_networking_secgroup_rule_v2" "rancher_agent" {
   direction         = "ingress"
   ethertype         = "tcp"
   security_group_id = openstack_networking_secgroup_v2.rancher_docker.id
-  port_range_min = 443
-  port_range_max = 443
+  port_range_min    = 443
+  port_range_max    = 443
 }
 
 resource "openstack_networking_secgroup_rule_v2" "ssh" {
   direction         = "egress"
   ethertype         = "tcp"
   security_group_id = openstack_networking_secgroup_v2.rancher_docker.id
-  port_range_min = 22
-  port_range_max = 22
+  port_range_min    = 22
+  port_range_max    = 22
 }
 
 resource "openstack_networking_secgroup_rule_v2" "docker_daemon" {
   direction         = "egress"
   ethertype         = "tcp"
   security_group_id = openstack_networking_secgroup_v2.rancher_docker.id
-  port_range_max = 2376
-  port_range_min = 2376
+  port_range_max    = 2376
+  port_range_min    = 2376
 }
 
 resource "openstack_networking_secgroup_rule_v2" "kubernetes_api" {
   direction         = "egress"
   ethertype         = "tcp"
   security_group_id = openstack_networking_secgroup_v2.rancher_docker.id
-  port_range_min = 6443
-  port_range_max = 6443
+  port_range_min    = 6443
+  port_range_max    = 6443
 }
--- server.tf
+++ server.tf
@@ -1,27 +1,30 @@
 resource "openstack_compute_instance_v2" "nodes" {
-    name            = var.node_name
-    image_id        = var.image_id
-    flavor_id       = var.flavor_id
-    key_pair        = openstack_compute_keypair_v2.deployer_keypair.name
-    security_groups = ["default", openstack_networking_secgroup_v2.rke.id]
+  count           = var.node_count
+  name            = "${var.node_name}-${count.index+1}"
+  image_id        = var.image_id
+  flavor_id       = var.flavor_id
+  key_pair        = openstack_compute_keypair_v2.deployer_keypair.name
+  security_groups = [
+    "default",
+    openstack_networking_secgroup_v2.rke.id]
 
-    metadata = {
-        this = "that"
-    }
+  metadata = {
+    this = "that"
+  }
 
-    network {
-        name = var.network
-    }
+  network {
+    name = var.network
+  }
 }
 
 resource "openstack_blockstorage_volume_v2" "node_storage" {
-    count = var.node_count
-    name  = "Node Storage"
-    size  = 100
+  count = var.node_count
+  name  = "Node Storage"
+  size  = 100
 }
 
-resource "openstack_compute_volume_attach_v2" "attached"{
-    count       = var.node_count
-    instance_id = openstack_compute_instance_v2.nodes[count.index].id
-    volume_id   = openstack_blockstorage_volume_v2.node_storage[count.index].id
+resource "openstack_compute_volume_attach_v2" "attached" {
+  count       = var.node_count
+  instance_id = openstack_compute_instance_v2.nodes[count.index].id
+  volume_id   = openstack_blockstorage_volume_v2.node_storage[count.index].id
 }
